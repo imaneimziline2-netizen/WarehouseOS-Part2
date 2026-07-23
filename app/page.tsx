00d7fb4b1ae157a324508229c27a766b66619a57
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-07-23 14:58:30 +0100

    feat: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import { auth } from "@/lib/auth";
+import DashboardContent from "@/components/dashboard/DashboardContent";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/login");
+export default async function DashboardPage() {
+    redirect("/dashboard");
+    // return <DashboardContent session={session} />;
 }
