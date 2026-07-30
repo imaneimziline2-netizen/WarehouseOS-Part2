183436ccb59d8f57376afcc45b914b4281015880
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-07-30 19:59:58 +0100

    add product creation form

---
+++ app/products/create/page.tsx
@@ -0,0 +1,9 @@
+import ProductForm from "@/components/forms/ProductForm";
+
+export default function CreateProductPage() {
+  return (
+    <main className="mx-auto max-w-3xl p-6">
+      <ProductForm />
+    </main>
+  );
+}
