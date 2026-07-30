91667a556d6b92b729a4beef27abc06bb28a6969
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-07-30 19:57:44 +0100

    add validation schemas for products and categories

--- lib/validations.ts
+++ lib/validations.ts
@@ -20,3 +20,45 @@ export const registerSchema = z
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
+
+
+
+export const productSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Product name must be at least 3 characters"),
+
+  sku: z
+    .string()
+    .min(3, "SKU is required"),
+
+  description: z
+    .string()
+    .min(5, "Description is required"),
+
+  category: z
+    .string()
+    .min(1, "Category is required"),
+
+  price: z
+    .number({
+      error: "Price is required",
+    })
+    .min(0),
+
+  quantity: z
+    .number({
+      error: "Quantity is required",
+    })
+    .min(0),
+});
+
+export const categorySchema = z.object({
+  name: z
+    .string()
+    .min(3, "Category name must be at least 3 characters"),
+
+  description: z
+    .string()
+    .min(5, "Description is required"),
+});
